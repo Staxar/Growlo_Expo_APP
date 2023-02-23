97b43c7cc4f88aefb727bc71623274f6fedf0a6a
madomaga <marcin.domagala@capgemini.com>
2023-02-23 14:01:34 +0100

    Initial commit, prepare structure

---
+++ src/styles/globalstyles.js
@@ -0,0 +1,16 @@
+export const GlobalStyles = {
+  colors: {
+    primary50: "#e4d9fd",
+    primary100: "#2db064",
+    primary200: "#a281f0",
+    primary400: "#5721d4",
+    primary500: "#3e04c3",
+    primary700: "#2d0689",
+    primary800: "#200364",
+    accent500: "#f7bc0c",
+    error50: "#fcc4e4",
+    error500: "#9b095c",
+    darkgray: "#171717",
+    lightgray: "#D6D6D6",
+  },
+};
