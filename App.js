89034190788beeb21c7a3da036cc40871da4ff2e
madomaga <marcin.domagala@capgemini.com>
2023-02-23 15:40:00 +0100

    x

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
 function AuthStack() {
   return (
     <Stack.Navigator
